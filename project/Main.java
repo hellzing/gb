359a08f3c096e40336796df8c668a364b05ec196
hellzing <grimpru@gmail.com>
2022-06-16 15:37:12 +0400

    homework7_jc

---
+++ project/Main.java
@@ -0,0 +1,9 @@
+package lesson7.project;
+
+public class Main {
+    public static void main(String[] args) {
+        lesson7.project.UserInterfaceView userInterfaceView = new lesson7.project.UserInterfaceView();
+
+        userInterfaceView.runInterface();
+    }
+}
