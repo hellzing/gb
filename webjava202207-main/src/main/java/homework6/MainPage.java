f890c1b2e1bb22cde9d3e7053744845cdbc0ac66
hellzing <grimpru@gmail.com>
2022-08-19 15:36:54 +0400

    homework6
    homework6

---
+++ webjava202207-main/src/main/java/homework6/MainPage.java
@@ -0,0 +1,27 @@
+package homework6;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+
+public class MainPage {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+    Actions actions;
+
+    public MainPage(WebDriver driver){
+        this.driver=driver;
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        actions = new Actions(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+@FindBy(xpath = "//a[contains(@href,'transport')]")
+    public WebElement transportSectionLink;
+
+}
